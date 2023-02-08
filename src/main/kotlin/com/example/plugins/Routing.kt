9e908a7a4aeaa3d6689f867b6885534281a1e512
Timothy Ghanim <timothy.ghanim@hotmail.com>
2023-02-08 16:09:41 -0500

    adding legacy source code

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,190 @@
+package com.example.plugins
+
+import com.example.services.Database
+import io.ktor.http.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Book(val title: String, val isbn: String, val authors: Array<String>, val category: Int)
+
+fun Application.configureRouting() {
+    routing {
+        route("/book") {
+            get {
+                val result = mutableListOf<Book>()
+                Database.initialize()
+                Database.getConnection().use {
+                    it.createStatement().use { stmt ->
+                        val sql = "SELECT TITLE, ISBN, AUTHORS, CATEGORY FROM BOOK"
+                        val queryResult = stmt.executeQuery(sql)
+                        while (queryResult.next()) {
+                            val title = queryResult.getString("TITLE")
+                            val isbn = queryResult.getString("ISBN")
+                            val authors = queryResult.getString("AUTHORS")
+                            val category = queryResult.getInt("CATEGORY")
+                            result.add(Book(title, isbn, authors.split(",").toTypedArray(), category))
+                        }
+                    }
+                }
+                call.respond(result)
+            }
+            get("/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull()
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid id")
+                } else {
+                    val result = mutableListOf<Book>()
+                    Database.initialize()
+                    Database.getConnection().use {
+                        it.createStatement().use { stmt ->
+                            val sql = "SELECT TITLE, ISBN, AUTHORS, CATEGORY FROM BOOK WHERE ID=${id}"
+                            val queryResult = stmt.executeQuery(sql)
+                            while (queryResult.next()) {
+                                val title = queryResult.getString("TITLE")
+                                val isbn = queryResult.getString("ISBN")
+                                val authors = queryResult.getString("AUTHORS")
+                                val category = queryResult.getInt("CATEGORY")
+                                result.add(Book(title, isbn, authors.split(",").toTypedArray(), category))
+                            }
+                        }
+                    }
+                    call.respond(result)
+                }
+            }
+            put("/{id}") {
+                val bookId = call.parameters["id"]?.toIntOrNull()
+                val operation = call.parameters["operation"]
+                val userId = call.request.queryParameters["userId"]?.toIntOrNull()
+                if (bookId == null || operation == null || userId == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid bookId or userId")
+                } else {
+                    Database.initialize()
+                    Database.getConnection().use {
+                        it.createStatement().use { stmt ->
+                            if (operation == "rent") {
+                                val sql = "UPDATE BOOK SET RENTER_ID=${userId} WHERE ID=${bookId}"
+                                stmt.execute(sql)
+                            } else if (operation == "return") {
+                                val sql = "UPDATE BOOK SET RENTER_ID=null WHERE ID=$bookId"
+                                stmt.execute(sql)
+                            } else {
+                            }
+                        }
+                    }
+                }
+            }
+            post {
+                val book = call.receive<Book>()
+                Database.initialize()
+                Database.getConnection().use {
+                    it.createStatement().use { stmt ->
+                        val id = (Database.getBookCount() ?: 0) + 1
+                        val sql =
+                            "INSERT INTO BOOK VALUES " +
+                            "(${id}, '${book.title}', " +
+                            "'${book.isbn}', " +
+                            "'${book.authors.joinToString(",")}', " +
+                            "${book.category})"
+                        stmt.execute(sql)
+                    }
+                }
+            }
+        }
+        route("/user") {
+            get {
+                val result = mutableListOf<String>()
+                Database.initialize()
+                Database.getConnection().use {
+                    it.createStatement().use { stmt ->
+                        val sql = "SELECT NAME FROM USER"
+                        val queryResult = stmt.executeQuery(sql)
+                        while (queryResult.next()) {
+                            result.add(queryResult.getString("NAME"))
+                        }
+                    }
+                }
+                call.respond(result)
+            }
+            get("/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull()
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid id")
+                } else {
+                    val result = mutableListOf<String>()
+                    Database.initialize()
+                    Database.getConnection().use {
+                        it.createStatement().use { stmt ->
+                            val sql = "SELECT NAME FROM USER WHERE ID=${id}"
+                            val queryResult = stmt.executeQuery(sql)
+                            while (queryResult.next()) {
+                                result.add(queryResult.getString("NAME"))
+                            }
+                        }
+                    }
+                    call.respond(result)
+                }
+            }
+            post {
+                val name = call.receiveText()
+                Database.initialize()
+                Database.getConnection().use {
+                    it.createStatement().use { stmt ->
+                        val id = (Database.getUserCount() ?: 0) + 1
+                        val sql = "INSERT INTO USER VALUES (${id}, '${name}')"
+                        stmt.execute(sql)
+                    }
+                }
+            }
+        }
+        route("/category") {
+            get {
+                val result = mutableListOf<String>()
+                Database.initialize()
+                Database.getConnection().use {
+                    it.createStatement().use { stmt ->
+                        val sql = "SELECT NAME FROM CATEGORY"
+                        val queryResult = stmt.executeQuery(sql)
+                        while (queryResult.next()) {
+                            result.add(queryResult.getString("NAME"))
+                        }
+                    }
+                }
+                call.respond(result)
+            }
+            get("/{id}") {
+                val id = call.parameters["id"]?.toIntOrNull()
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid id")
+                } else {
+                    val result = mutableListOf<String>()
+                    Database.initialize()
+                    Database.getConnection().use {
+                        it.createStatement().use { stmt ->
+                            val sql = "SELECT NAME FROM CATEGORY WHERE ID=${id}"
+                            val queryResult = stmt.executeQuery(sql)
+                            while (queryResult.next()) {
+                                result.add(queryResult.getString("NAME"))
+                            }
+                        }
+                    }
+                    call.respond(result)
+                }
+            }
+            post {
+                val name = call.receiveText()
+                Database.initialize()
+                Database.getConnection().use {
+                    it.createStatement().use { stmt ->
+                        val id = (Database.getCategoryCount() ?: 0) + 1
+                        val sql = "INSERT INTO CATEGORY VALUES (${id}, '${name}')"
+                        stmt.execute(sql)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/example/services/Database.kt
@@ -0,0 +1,105 @@
+package com.example.services
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class Database {
+    companion object {
+        fun getUserCount(): Int? {
+            initialize()
+            getConnection().use {
+                it.createStatement().use { stmt ->
+                    val sql = "SELECT COUNT(*) FROM USER"
+                    val queryResult = stmt.executeQuery(sql)
+                    while (queryResult.next()) {
+                        return queryResult.getInt(1)
+                    }
+                }
+            }
+            return null
+        }
+        fun getCategoryCount(): Int? {
+            initialize()
+            getConnection().use {
+                it.createStatement().use { stmt ->
+                    val sql = "SELECT COUNT(*) FROM CATEGORY"
+                    val queryResult = stmt.executeQuery(sql)
+                    while (queryResult.next()) {
+                        return queryResult.getInt(1)
+                    }
+                }
+            }
+            return null
+        }
+        fun getBookCount(): Int? {
+            initialize()
+            getConnection().use {
+                it.createStatement().use { stmt ->
+                    val sql = "SELECT COUNT(*) FROM BOOK"
+                    val queryResult = stmt.executeQuery(sql)
+                    while (queryResult.next()) {
+                        return queryResult.getInt(1)
+                    }
+                }
+            }
+            return null
+        }
+        fun initialize() {
+            val connection = getConnection()
+            connection.use { connection ->
+                val tables = getTables(connection)
+                if (!tables.contains("CATEGORY")) {
+                    createCategoryTable(connection)
+                }
+                if (!tables.contains("USER")) {
+                    createUserTable(connection)
+                }
+                if (!tables.contains("BOOK")) {
+                    createBookTable(connection)
+                }
+            }
+        }
+        private fun getTables(conn: Connection): Array<String> {
+            val stmt = conn.createStatement()
+            val sql = "select name from sqlite_schema where type = 'table'"
+            val queryResult = stmt.executeQuery(sql)
+            val result: MutableList<String> = mutableListOf()
+            while (queryResult.next()) {
+                result.add(queryResult.getString("name"))
+            }
+            stmt.close()
+            return result.toTypedArray()
+        }
+        private fun createCategoryTable(conn: Connection): Unit {
+            val stmt = conn.createStatement()
+            val sql = "CREATE TABLE CATEGORY " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL)"
+            stmt.executeUpdate(sql)
+            stmt.close()
+        }
+        private fun createUserTable(conn: Connection): Unit {
+            val stmt = conn.createStatement()
+            val sql = "CREATE TABLE USER " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL)"
+            stmt.executeUpdate(sql)
+            stmt.close()
+        }
+        private fun createBookTable(conn: Connection): Unit {
+            val stmt = conn.createStatement()
+            val sql = "CREATE TABLE BOOK " +
+                    "(ID INT PRIMARY KEY    NOT NULL," +
+                    " TITLE          TEXT   NOT NULL," +
+                    " ISBN           TEXT   NOT NULL," +
+                    " AUTHORS        TEXT   NOT NULL," +
+                    " CATEGORY       INT   NULL)"
+            stmt.executeUpdate(sql)
+            stmt.close()
+        }
+        fun getConnection(): Connection {
+            Class.forName("org.sqlite.JDBC")
+            return DriverManager.getConnection("jdbc:sqlite:test.db")
+        }
+    }
+}
