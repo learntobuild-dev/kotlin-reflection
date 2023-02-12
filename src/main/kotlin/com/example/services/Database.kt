fd8801959bb5e8a6147635cc7200d0a9e93f7004
Timothy Ghanim <timothy.ghanim@hotmail.com>
2023-02-12 16:08:48 -0500

    delete manual getCount for entities.

--- src/main/kotlin/com/example/services/Database.kt
+++ src/main/kotlin/com/example/services/Database.kt
@@ -1,55 +1,10 @@
 package com.example.services
 
-import com.example.datamodel.DatabaseContext
 import java.sql.Connection
 import java.sql.DriverManager
 
 class Database {
     companion object {
-        fun getUserCount(): Int? {
-            val connection = getConnection()
-            DatabaseContext.ensureCreated(connection)
-            connection.use {
-                it.createStatement().use { stmt ->
-                    val sql = "SELECT COUNT(*) FROM USER"
-                    val queryResult = stmt.executeQuery(sql)
-                    while (queryResult.next()) {
-                        return queryResult.getInt(1)
-                    }
-                }
-            }
-            return null
-        }
-
-        fun getCategoryCount(): Int? {
-            val connection = getConnection()
-            DatabaseContext.ensureCreated(connection)
-            connection.use {
-                it.createStatement().use { stmt ->
-                    val sql = "SELECT COUNT(*) FROM CATEGORY"
-                    val queryResult = stmt.executeQuery(sql)
-                    while (queryResult.next()) {
-                        return queryResult.getInt(1)
-                    }
-                }
-            }
-            return null
-        }
-
-        fun getBookCount(): Int? {
-            val connection = getConnection()
-            DatabaseContext.ensureCreated(connection)
-            connection.use {
-                it.createStatement().use { stmt ->
-                    val sql = "SELECT COUNT(*) FROM BOOK"
-                    val queryResult = stmt.executeQuery(sql)
-                    while (queryResult.next()) {
-                        return queryResult.getInt(1)
-                    }
-                }
-            }
-            return null
-        }
 
         fun getConnection(): Connection {
             Class.forName("org.sqlite.JDBC")
