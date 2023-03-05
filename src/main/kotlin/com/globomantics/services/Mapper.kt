10ee5146f8f1564b74b800294cfc44e1125d0aa3
Timothy Ghanim <timothy.ghanim@hotmail.com>
2023-03-05 11:06:22 -0500

    refactoring

--- src/main/kotlin/com/globomantics/services/Mapper.kt
+++ src/main/kotlin/com/globomantics/services/Mapper.kt
@@ -2,6 +2,7 @@ package com.example.services
 
 import kotlin.reflect.*
 import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.memberProperties
 
 class Mapper {
     companion object {
@@ -15,46 +16,47 @@ class Mapper {
             source: Any?,
             sourceType: KClass<*>,
             destinationType: KClass<*>) : Any {
-            val descriptors: MutableList<ArgumentDescriptor> = mutableListOf()
-            for (member in sourceType.members) {
-                if (member is KProperty0<*> ||
-                    member is KProperty1<*, *> ||
-                    member is KProperty2<*, *, *>) {
-                    val propertyType = member.returnType
-                    val propertyValue = member.call(source)
-                    descriptors.add(ArgumentDescriptor(member.name, propertyType, propertyValue))
-                }
+            val arguments: MutableList<Argument> = mutableListOf()
+            for (member in sourceType.memberProperties) {
+                val propertyType = member.returnType
+                val propertyValue = member.call(source)
+                arguments.add(Argument(member.name, propertyType, propertyValue))
             }
-            val ctors = findConstructor(destinationType, descriptors);
+            val ctors = findConstructor(destinationType, arguments);
             if (ctors.isNotEmpty()) {
                 val sorted =
                     ctors
                         .filter { it.parameters.isNotEmpty() }
                         .sortedByDescending { it.parameters.size }
                 for (ctor in sorted) {
-                    val arguments: MutableList<Any?> = mutableListOf();
+                    val finalArguments: MutableList<Any?> = mutableListOf()
+                    var hasAllArguments = true
                     for (parameter in ctor.parameters) {
-                        val descriptor =
-                            descriptors.firstOrNull { d -> d.name == parameter.name }
-                                ?: continue
+                        val descriptor = arguments.firstOrNull { d -> d.name == parameter.name }
+                        if (descriptor == null) {
+                            hasAllArguments = false
+                            break
+                        }
                         if (parameter.type == descriptor.type) {
-                            arguments.add(descriptor.value)
+                            finalArguments.add(descriptor.value)
                         } else {
                             val argumentClass = descriptor.type.classifier as KClass<*>
                             val parameterClass = parameter.type.classifier as KClass<*>
                             val argument = map(descriptor.value, argumentClass, parameterClass)
-                            arguments.add(argument)
+                            finalArguments.add(argument)
                         }
                     }
-                    var argumentsArray = arguments.toTypedArray()
-                    return ctor.call(*argumentsArray)
+                    if (hasAllArguments) {
+                        var argumentsArray = finalArguments.toTypedArray()
+                        return ctor.call(*argumentsArray)
+                    }
                 }
             }
             val parameterlessCtor =
                 ctors.firstOrNull { it.parameters.isEmpty() }
                     ?: throw Exception("Could not find a parameterless constructor")
             val newInstance = parameterlessCtor.call()
-            for (argument in descriptors) {
+            for (argument in arguments) {
                 var valueSet = false
                 for (property in destinationType.declaredMemberProperties) {
                     if (property.name == argument.name) {
@@ -79,7 +81,7 @@ class Mapper {
 
         fun <T : Any> findConstructor(
             typeClass: KClass<T>,
-            arguments: List<ArgumentDescriptor>): Array<KFunction<T>> {
+            arguments: List<Argument>): Array<KFunction<T>> {
             val result = mutableListOf<KFunction<T>>()
             for (constructor in typeClass.constructors) {
                 if (compatible(constructor.parameters, arguments)) {
@@ -91,7 +93,7 @@ class Mapper {
 
         private fun compatible(
             parameters: List<KParameter>,
-            arguments: List<ArgumentDescriptor>): Boolean {
+            arguments: List<Argument>): Boolean {
             for (parameter in parameters) {
                 var hasMatchingArgument: Boolean = false
                 for (argument in arguments) {
@@ -108,7 +110,7 @@ class Mapper {
         }
     }
 
-    class ArgumentDescriptor(
+    class Argument(
         val name: String, val type: KType, val value: Any?
     )
 }
